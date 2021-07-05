10c650e6aa4041af0bbaa3da386c87158ed099ba
Denis Onder <denis_onder@protonmail.com>
2021-07-05 21:52:33 +0200

    Proper error handling for the user builder

--- validators/user_validator.go
+++ validators/user_validator.go
@@ -1,35 +1,36 @@
 package validators
 
 import (
+	"errors"
 	"strings"
 
 	. "github.com/denis-onder/archibald/domain"
 )
 
-func ValidateUser(u User) bool {
+func ValidateUser(u User) error {
 	if u.GetFirstName() == "" {
-		return false
+		return errors.New("First name is required.")
 	}
 
 	if u.GetLastName() == "" {
-		return false
+		return errors.New("Last name is required.")
 	}
 
 	if u.GetDisplayName() == "" {
-		return false
+		return errors.New("Display name is required.")
 	}
 
 	if u.GetEmail() == "" {
-		return false
+		return errors.New("An email address is required.")
 	}
 
 	if !strings.Contains(u.GetEmail(), "@mail.com") {
-		return false
+		return errors.New("Please provide a valid email address")
 	}
 
 	if u.GetAge() < 0 {
-		return false
+		return errors.New("Age must be a positive number")
 	}
 
-	return true
+	return nil
 }

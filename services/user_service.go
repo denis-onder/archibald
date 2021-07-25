57a56bdda76074257c3452daf03d7c46ef5f8c00
Denis Onder <denis_onder@protonmail.com>
2021-07-25 11:48:13 +0200

    Working on the user service and controller

---
+++ services/user_service.go
@@ -0,0 +1,16 @@
+package services
+
+import (
+	. "github.com/denis-onder/archibald/builders"
+	. "github.com/denis-onder/archibald/domain"
+	. "github.com/denis-onder/archibald/validators"
+)
+
+func CreateUser(firstName, lastName, email string, age int) (error, User) {
+	user := UserBuilder(firstName, firstName, email, age)
+	isValid := ValidateUser(user)
+
+	// TODO: Insert the user in the database if valid
+
+	return isValid, user
+}

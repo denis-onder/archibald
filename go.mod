11be7c7302cabbb3b154c10e2602d62c1b9eb2c1
Denis Onder <denis_onder@protonmail.com>
2021-08-05 15:27:10 +0200

    Environment config file, database conenction setup

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/denis-onder/archibald
 
 go 1.16
+
+require (
+	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/lib/pq v1.10.2 // indirect
+)
--- main.go
+++ main.go
@@ -2,7 +2,16 @@ package main
 
 import (
 	"fmt"
+	"log"
+
+	"github.com/joho/godotenv"
 )
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Printf("No environmental variable (.env) file found.")
+	}
+}
 
 func main() {
 	fmt.Println("Archibald started")

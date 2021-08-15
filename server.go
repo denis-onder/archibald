75252d5d17adea7c5d19704368b2436026d249d5
Denis Onder <denis_onder@protonmail.com>
2021-08-15 14:44:43 +0200

    Writing a logger function to log requests

--- server.go
+++ server.go
@@ -5,10 +5,16 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"time"
 )
 
 func welcome(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Welcome!")
+}
+
+func logger(w http.ResponseWriter, r *http.Request) {
+	// Method, endpoint and timestamp
+	fmt.Printf("=> [%s] %s @%s\n", r.Method, r.URL.Host, time.Now().Unix())
 }
 
 func serve() {
@@ -17,6 +23,7 @@ func serve() {
 	fmt.Printf("Archibald service running\nhttp://localhost%s/welcome", port)
 
 	mux := http.NewServeMux()
+	mux.HandleFunc("/", logger)
 	mux.HandleFunc("/welcome", welcome)
 
 	http.ListenAndServe(port, mux)

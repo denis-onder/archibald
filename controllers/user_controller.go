5280c80d8d7d2acb82f9db58c5d132840a43462a
Denis Onder <denis_onder@protonmail.com>
2021-08-26 21:56:02 +0200

    User service and controller

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -1,8 +1,18 @@
 package controllers
 
-import . "github.com/denis-onder/archibald/domain"
+import (
+	. "github.com/denis-onder/archibald/domain"
+	UserService "github.com/denis-onder/archibald/services"
+)
 
-func CreateUser(user User) bool {
+func CreateUser(firstName, lastName, email string, age int) User {
+	err, user := UserService.CreateUser(firstName, lastName, email, age)
+
+	if err != nil {
+		// Handle error
+	}
+
+	return user
 }
 
 func GetUser(id int) User {
--- services/user_service.go
+++ services/user_service.go
@@ -6,6 +6,13 @@ import (
 	. "github.com/denis-onder/archibald/validators"
 )
 
+type UserServiceType struct {
+	CreateUser func(firstName, lastName, email string, age int) (error, User)
+	GetUser    func(id int) (error, User)
+	UpdateUser func(id int, updateUserObject User) bool
+	DeleteUser func(id int) bool
+}
+
 func CreateUser(firstName, lastName, email string, age int) (error, User) {
 	user := UserBuilder(firstName, firstName, email, age)
 	isValid := ValidateUser(user)
@@ -15,11 +22,29 @@ func CreateUser(firstName, lastName, email string, age int) (error, User) {
 	return isValid, user
 }
 
-func GetUser(id int) User {
+func GetUser(id int) (error, User) {
+	// Database query
+	// If no user found return error
+	// Return user
 }
 
 func UpdateUser(id int, updateUserObject User) bool {
+	// Database query
+	// If no user found return error
+	// Validate user with modified data
+	// If validation error, return error
+	// Return user
 }
 
 func DeleteUser(id int) bool {
+	// Database query
+	// If no user found return false
+	// Return true
+}
+
+var UserService UserServiceType = UserServiceType{
+	CreateUser: CreateUser,
+	GetUser:    GetUser,
+	UpdateUser: UpdateUser,
+	DeleteUser: DeleteUser,
 }
